31a53428cc3db5c17125ba6022dee02ed6f219ca
Zhang Phillip <pzhang@lcs-mba3156.lcs.on.ca>
2017-10-14 10:25:10 -0400

    finished colouring in the mountain

--- Variables_And_Iteration/Variables_And_Iteration.playground/Pages/Exercise.xcplaygroundpage/Contents.swift
+++ Variables_And_Iteration/Variables_And_Iteration.playground/Pages/Exercise.xcplaygroundpage/Contents.swift
@@ -71,17 +71,34 @@ for i in stride(from: 100, through: 150, by:1) {
 
 
 //Draw a line
-
+canvas.defaultLineWidth=1
 canvas.drawLine(fromX: 150, fromY: 140, toX: 100, toY: 195)
+for i in stride(from: 150, through: 200, by:1) {
+    
+    //Draw each line
+    canvas.drawLine(fromX: i, fromY: 0, toX: i, toY: -5+i)
+}
 
 //Draw a line
+canvas.defaultLineWidth=1
 canvas.drawLine(fromX: 150, fromY: 140, toX: 200, toY: 195)
+for i in stride(from: 200, through: 250, by:1) {
+    
+    //Draw each line
+    canvas.drawLine(fromX: i, fromY: 0, toX: i, toY: 395-i)
+}
 
 //Draw a line
+canvas.defaultLineWidth=1
 canvas.drawLine(fromX: 200, fromY: 195, toX: 250, toY: 140)
-
+for i in stride(from: 250, through: 300, by:1) {
+    
+    //Draw each line
+    canvas.drawLine(fromX: i, fromY: 0, toX: i, toY: -105+i)
+}
 //Draw a line
 canvas.drawLine(fromX: 250, fromY: 140, toX: 300, toY: 195)
+
 
 
 /*:
